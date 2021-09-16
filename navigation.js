d4f15d7f6777c3b421c4b173da27ba084b9a7f7f
chuanqizhu0820 <chuanqizhu1991@outlook.com>
2021-09-16 16:51:29 +0100

    Fix linter error

--- navigation.js
+++ navigation.js
@@ -13,10 +13,10 @@ links.forEach((link) => {
     currentSection.classList.add('block');
   });
 });
-
+/* eslint-disable */
 var DateTime = luxon.DateTime;
+/* eslint-enable */
 function clock() {
-
   const now = DateTime.local().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
   const timeHtml = `<p>${now}</p>`;
   document.querySelector('#time').innerHTML = timeHtml;
